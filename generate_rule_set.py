7068f2ec8e7efac6a07467d564be1253eaa1ba4a
sieveLau <sievelau@gmail.com>
2024-01-23 23:56:06 +0800

    Update generate_rule_set.py
    only focus on ip rules

--- generate_rule_set.py
+++ generate_rule_set.py
@@ -7,9 +7,6 @@ import os
 from aggregate6 import aggregate
 
 dnsmasq_china_list = [
-    "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf",
-    "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf",
-    "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf"
 ]
 
 chnroutes2 = [
@@ -21,11 +18,10 @@ apnic = [
 ]
 
 maxmind = [
-    "https://raw.githubusercontent.com/Dreamacro/maxmind-geoip/release/Country.mmdb"
+    https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb
 ]
 
 adguard = [
-    "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
 ]
 
 output_dir = "./rule-set"
